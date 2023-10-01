64f1302fb0cc987a0057ffb7646b500f0d36194d
bjoernhansen <hansen_bjoern@web.de>
2023-10-02 00:21:54 +0200

    simplified GraphicsApiType

--- src/main/java/de/helicopter_vs_aliens/graphics/GraphicsApiType.java
+++ src/main/java/de/helicopter_vs_aliens/graphics/GraphicsApiType.java
@@ -9,30 +9,21 @@ import java.util.function.Supplier;
 
 public enum GraphicsApiType
 {
-    GRAPHICS_2D(AwtController::getDisplayShift, 12),
-    JAVAFX(GameApplication::getDisplayShift, 17);
+    GRAPHICS_2D(AwtController::getDisplayShift),
+    JAVAFX(GameApplication::getDisplayShift);
 
 
     private final Supplier<Dimension>
         displayShiftSupplier;
 
-    private final int
-        boldThreshold;
 
-
-    GraphicsApiType(Supplier<Dimension> displayShiftSupplier, int boldThreshold)
+    GraphicsApiType(Supplier<Dimension> displayShiftSupplier)
     {
         this.displayShiftSupplier = displayShiftSupplier;
-        this.boldThreshold = boldThreshold;
     }
 
     public Dimension getDisplayShift()
     {
         return displayShiftSupplier.get();
-    }
-
-    public int getBoldThreshold()
-    {
-        return boldThreshold;
     }
 }
