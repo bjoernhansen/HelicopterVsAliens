4b40c329936b2be5da659faf40c879ae55f18fc7
bjoernhansen <hansen_bjoern@web.de>
2025-12-07 12:27:34 +0100

    Notizen ergänzt

--- src/main/java/de/helicopter_vs_aliens/graphics/painter/Painter.java
+++ src/main/java/de/helicopter_vs_aliens/graphics/painter/Painter.java
@@ -7,4 +7,23 @@ import de.helicopter_vs_aliens.model.Paintable;
 public abstract class Painter<E extends Paintable>
 {
      public abstract void paint(GraphicsAdapter graphicsAdapter, E paintableEntity);
+     
+     /*
+     // TODO Abwägen, ob dies eine sinnvolle Änderung wäre
+          private GraphicsAdapter graphicsAdapter;
+     
+     public void paint(GraphicsAdapter graphicsAdapter, E paintableEntity)
+     {
+          this.graphicsAdapter = graphicsAdapter;
+          paint(paintableEntity);
+     }
+     
+     protected abstract void paint(E paintableEntity);
+     
+     protected GraphicsAdapter getGraphicsAdapter()
+     {
+          return graphicsAdapter;
+     }
+     
+      */
 }
